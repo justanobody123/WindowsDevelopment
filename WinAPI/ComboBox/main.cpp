d3b11bb8ee0e5d32157fd6453b51f39b38ba84eb
Кристина Волосова <justanobody017527@gmail.com>
2024-07-30 22:29:43 +0300

    Homework DONE

--- WinAPI/ComboBox/main.cpp
+++ WinAPI/ComboBox/main.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include "resource.h"
+#include <string.h>
 
 CONST CHAR* values[] = { "This", "is", "my", "first", "Combo", "Box" };
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -21,9 +22,36 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 			SendMessage(hCombo, CB_ADDSTRING, 0, (LPARAM)values[i]);
 		}
-	}	
-		break;
+	}
+	break;
 	case WM_COMMAND:
+
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+		{
+			CONST INT SIZE = 256;
+			CHAR output[SIZE]{ "Вы не выбрали какой-либо пункт." };
+			HWND hCB = GetDlgItem(hwnd, IDC_COMBO1);
+			INT index = SendMessage(hCB, CB_GETCURSEL, 0, 0);
+
+			if (index != CB_ERR)
+			{
+				CHAR indexStr[20];
+				CHAR selectedItem[SIZE];
+				SendMessage(hCB, CB_GETLBTEXT, index, (LPARAM)selectedItem); //GET LIST BOX TEXT
+				strcpy_s(output, SIZE, "Вы выбрали пункт №");
+				_itoa_s(index, indexStr, 10);
+				strcat_s(output, SIZE, indexStr);
+				strcat_s(output, SIZE, " с содержимым ");
+				strcat_s(output, SIZE, selectedItem);
+			}
+			
+			MessageBox(hwnd, output, "Output", MB_OK);
+			break;
+		}
+
+		}
 		break;
 	case WM_CLOSE:
 		EndDialog(hwnd, 0);
