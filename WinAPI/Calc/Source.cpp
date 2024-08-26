287cfacaf8e1f14c1170bbbff1db671bdaba1d59
Кристина Волосова <justanobody017527@gmail.com>
2024-08-26 16:13:15 +0300

    Calc DONE.

--- WinAPI/Calc/Source.cpp
+++ WinAPI/Calc/Source.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits>
 
 CONST INT g_i_SLEEP_TIME = 100;
 
@@ -153,6 +154,10 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	break;
 	case WM_COMMAND:
 	{
+		static double a = DBL_MIN, b = 0;
+		static int operation = 0;
+		static bool input = false;
+		static bool operation_input = false;
 		SetFocus(hwnd);
 		CONST INT SIZE = 256;
 		CHAR sz_display[SIZE]{};
@@ -160,6 +165,8 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HWND hEditDisplay = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
 		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_POINT)
 		{
+			if (!input) SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)"");
+
 			sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
 			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
 			if (LOWORD(wParam) == IDC_BUTTON_POINT)
@@ -171,6 +178,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			{
 				if (sz_display[0] == '0' && strlen(sz_display) == 1)sz_display[0] = sz_digit[0];
 				else strcat(sz_display, sz_digit);
+				input = true;
 			}
 			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
 		}
@@ -185,37 +193,74 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		}
 		if (LOWORD(wParam) == IDC_BUTTON_CLEAR)
 		{
+			a = DBL_MIN, b = 0, operation = 0, input = false, operation_input = false;
 			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)"0");
 		}
-		if (LOWORD(wParam) == IDC_BUTTON_PLUS || LOWORD(wParam) == IDC_BUTTON_MINUS || LOWORD(wParam) == IDC_BUTTON_ASTER || LOWORD(wParam) == IDC_BUTTON_SLASH || LOWORD(wParam) == IDC_BUTTON_EQUAL)
+		if (LOWORD(wParam) >= IDC_BUTTON_PLUS && LOWORD(wParam) <= IDC_BUTTON_SLASH)
 		{
 			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+			if (input && a == DBL_MIN)
+			{
+				a = atof(sz_display);
+				input = false;
+			}
+			//else b = atof(sz_display);
+			if (input) SendMessage(hwnd, WM_COMMAND, IDC_BUTTON_EQUAL, 0);
+			//
+			
+			operation = LOWORD(wParam);
+			operation_input = true;
+			
 
-				if (IsParseable(sz_display))
-				{
-					//Заменяем содержимое дисплея на результат мат. операции
-					SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)Parse(sz_display));
-					SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
-				}
-				if (LOWORD(wParam) == IDC_BUTTON_PLUS)
-				{
-					strcat(sz_display, "+");
-				}
-				else if (LOWORD(wParam) == IDC_BUTTON_MINUS)
-				{
-					strcat(sz_display, "-");
-				}
-				else if (LOWORD(wParam) == IDC_BUTTON_ASTER)
-				{
-					strcat(sz_display, "*");
-				}
-				else if (LOWORD(wParam) == IDC_BUTTON_SLASH)
-				{
-					strcat(sz_display, "/");
-				}
-				SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+				//if (IsParseable(sz_display))
+				//{
+				//	//Заменяем содержимое дисплея на результат мат. операции
+				//	SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)Parse(sz_display));
+				//	SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+				//}
+				//if (LOWORD(wParam) == IDC_BUTTON_PLUS)
+				//{
+				//	strcat(sz_display, "+");
+				//}
+				//else if (LOWORD(wParam) == IDC_BUTTON_MINUS)
+				//{
+				//	strcat(sz_display, "-");
+				//}
+				//else if (LOWORD(wParam) == IDC_BUTTON_ASTER)
+				//{
+				//	strcat(sz_display, "*");
+				//}
+				//else if (LOWORD(wParam) == IDC_BUTTON_SLASH)
+				//{
+				//	strcat(sz_display, "/");
+				//}
+				//SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
 	
 			
+		}
+		if (LOWORD(wParam) == IDC_BUTTON_EQUAL)
+		{
+			SendMessage(hEditDisplay, WM_GETTEXT, SIZE, (LPARAM)sz_display);
+			if (input)b = atof(sz_display);
+			switch (operation)
+			{
+			case IDC_BUTTON_PLUS:
+				a += b;
+				break;
+			case IDC_BUTTON_MINUS:
+				a -= b;
+					break;
+			case IDC_BUTTON_ASTER:
+				a *= b;
+				break;
+			case IDC_BUTTON_SLASH:
+				a /= b;
+				break;
+			}
+			sprintf(sz_display, "%f", a);
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+			operation_input = false;
+			input = false;
 		}
 	}
 	/*switch (LOWORD(wParam))
